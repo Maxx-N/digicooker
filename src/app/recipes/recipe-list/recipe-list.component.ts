3762d1f30111671de61015f487a5151056c6f4e4
Maxime NORVAL <maximen@isimedia.intra>
2020-08-28 15:56:51 +0200

    creating the detail component template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe',
+      'This is only a sample',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
+    ),
+    new Recipe(
+      'Shrimp cocktail',
+      'Catch shrimps and cook them !',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    ),
+  ];
 
   constructor() {}
 
