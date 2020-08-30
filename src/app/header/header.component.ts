0bbeb9c799ac4e076a66d183233e01966c724f88
Maxime NORVAL <maximen@isimedia.intra>
2020-08-30 17:56:27 +0200

    making navigation through tabs working

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected: EventEmitter<string> = new EventEmitter<string>();
   collapsed = true;
   constructor() {}
   ngOnInit(): void {}
+
+  onSelect(feature: string): void {
+    this.featureSelected.emit(feature);
+  }
 }
